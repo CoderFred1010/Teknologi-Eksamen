44cd9b9aed740862e8272bbf00cf1ad1cb546796
Coderfred1010 <frederik.fuglo@gmail.com>
2025-12-29 17:24:50 +0100

    initial commit java spring boot project

---
+++ src/main/java/dk/ek/teknologieksamen/TeknologiEksamenApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.teknologieksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ek/teknologieksamen/TeknologiEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.teknologieksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
